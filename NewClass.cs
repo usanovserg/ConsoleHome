46d841423171bd54ecfc2ae3b4bda7bd80747443
serg-225 <Usanov.serg@gmail.com>
2023-12-09 08:48:24 +0300

    Изменил название команды на master

--- NewClass.cs
+++ NewClass.cs
@@ -10,7 +10,7 @@ namespace ConsoleHome
     {
         public NewClass()
         {
-            Console.WriteLine("Команда разработчиков 1!");
+            Console.WriteLine("Команда разработчиков master!");
         }
     }
 }
