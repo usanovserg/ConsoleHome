04ee969d1cd4b58b862a7167c0450542ef750535
serg-225 <Usanov.serg@gmail.com>
2023-12-11 20:27:08 +0300

    Создали класс Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+using System;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+
+        while (true)
+        {
+            Console.WriteLine("Введите число: ");
+            string? str = Console.ReadLine();
+
+
+            if (int.TryParse(str, out int num) == true)
+            {
+                if (num != 0)
+                {
+                    decimal count = 9 / num;
+
+                    Console.WriteLine("result = " + count);
+                }
+                else Console.WriteLine("На ноль делить нельзя!");
+            }
+            else  Console.WriteLine("введите корректное значение");
+        }
+
+    }
+}
+
+
+
+
