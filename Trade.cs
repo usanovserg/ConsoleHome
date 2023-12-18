0bcf6179c98099373fdbdaa03a072338f4cf9232
Galay <galay007@mail.ru>
2023-12-19 00:35:59 +0300

    Отправил ДЗ

---
+++ Trade.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5
+{
+    public class Trade
+    {
+        public int Volume;
+        public int Price;
+        
+        public int OpenPrice;
+        public int k;
+      
+        public enum Direction 
+        {
+            Long = 1,
+            Short = -1,
+            
+        }
+        private int OpenPos;
+
+        public int OpenPosition
+        {
+            get 
+            { 
+                return OpenPos;
+            }
+            set 
+            { 
+                OpenPos = OpenPos + value;
+                
+            }
+        }
+
+    }
+}
