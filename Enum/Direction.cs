53600ab03b598275febfa5d56da2e4dd4e16467e
serg-225 <Usanov.serg@gmail.com>
2023-12-18 20:15:05 +0300

    6165161

--- Enum/Direction.cs
+++ Enum/Direction.cs
@@ -12,6 +12,6 @@ namespace ConsoleHome.Enum
 
         None,
 
-        Buy
+        Buy 
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ConsoleHome;
+using ConsoleHome.Enum;
 
 namespace ConsoleHome
 {
@@ -18,13 +19,17 @@ namespace ConsoleHome
         {
             Position position = new Position(1000, "Position_1"); // создаем экземпляр класс Position
 
-            Position position2 = new Position(5000, "Position_2"); 
+            //Position position2 = new Position(5000, "Pos_2222222222222222"); 
 
             //Trade trade = new Trade(); // создаем экземпляр класс Trade 
 
-
+            int num = (int)Direction.Buy;
+            num = (int)Direction.Sell;
+            num = (int)Direction.None;
 
             Console.ReadLine();
+
+            
         }
     }
 }
