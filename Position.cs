c0616425a35632cda3e54b8568966b3f6cb4d46e
ivkt <ivkt@yandex.ru>
2023-12-24 18:24:30 +0300

    IgorKotik_lesson_05

---
+++ Position.cs
@@ -0,0 +1,47 @@
+﻿using ConsoleHome.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace ConsoleHome
+{
+    public class Position
+    {
+        public PositionTrend PositionTrend;
+        public decimal PriceOpen = 0;
+        public decimal PriceClose = 0;
+        public decimal StopLoss = 0;
+        public int volumeOpen = 0;
+        public int volumeClose = 0;
+        public Position()
+        {
+        Timer timer = new Timer();
+        timer.Interval = 3000;
+        timer.Elapsed += NewTrade;
+        timer.Start();
+        }
+
+        Random random = new Random();
+      
+        private void NewTrade(object sender, ElapsedEventArgs e)
+        {
+        Trade trade = new Trade();
+        int num = random.Next(-10, 10);
+            if (num > 0)
+            {
+              PositionTrend = PositionTrend.Long;   // long
+            }
+            else if (num <0)
+            {
+                PositionTrend = PositionTrend.Short;  //short
+            }
+            trade.Volume = Math.Abs(num);
+            trade.Price = random.Next(70000, 80000);
+            string str = "Volume = " + trade.Volume.ToString() + "/ Price = " + trade.Price.ToString() + "/ Направление сделки: "+ PositionTrend;
+            Console.WriteLine(str);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleHome
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Position position = new Position();
+            Console.ReadLine();
+        }
+
+
+    }
+}
