672f0f10ffdf60a0382023b0ef3978906c49893b
ivkt <ivkt@yandex.ru>
2024-01-02 21:29:40 +0300

    ДЗ к уроку 1-6

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleHome
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Position position = new Position();
+            position.PriceChangeEvents += PriceLine;//Добавляем обработчик для события PriceChangeEvents
+            Console.ReadLine();
+        }
+        //при вызове события PriceChangeEvents будет вызываться этот метод PriceLine,
+        //которому для параметра message будет передаваться строка, которая передается в PriceChangeEvents?.Invoke()
+
+        public static void PriceLine(string message)
+        {
+            Console.WriteLine($"Позиция изменилась. {message}");
+
+        }
+
+    }
+}
---
+++ Trade.cs
@@ -0,0 +1,50 @@
+﻿using ConsoleHome.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHome
+{
+    public class Trade
+    {
+        #region Fields
+        /// <summary>
+        /// Цена инструмента
+        /// </summary>
+
+        public decimal Price = 0;
+        public string SecCode = "";
+        public string ClassCode = "";
+        public DateTime DateTime = DateTime.MinValue;
+        public string Portfolio = "";
+        public PositionTrend PositionTrend;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Объем сделки
+        /// </summary>
+        public decimal Volume
+        {
+            get
+            {
+                return _volume;
+            }
+            set
+            {
+                _volume = value;
+            }
+
+        }
+        decimal _volume = 0;
+        #endregion
+
+
+
+    }
+    
+}
