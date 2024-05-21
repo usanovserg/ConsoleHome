e07708689ce32aae10fb746c1d05b7f1b8b7b525
tlddevice <zocus1@gmail.com>
2024-05-21 16:16:40 +0300

    Price levels console app copy

--- Program.cs
+++ Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleHome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Инициализация списка и ввод данных для расчета 
+
+            priceLevels = new List<decimal>();
+
+            WriteLines();
+
+            Console.Write("Задайте верхний уровень цены: ");
+
+            decimal topPrice = Decimal.Parse(Console.ReadLine());
+
+            Console.Write("Задайте нижний уровень цены: ");
+
+            decimal bottomPrice = Decimal.Parse(Console.ReadLine());
+
+            Console.Write("Задайте шаг цены: ");
+
+            decimal priceStep = Decimal.Parse(Console.ReadLine());
+
+            //Расчет сетки уровней
+
+            decimal priceLevel = topPrice;
+
+            while (priceLevel >= bottomPrice)
+            {
+                priceLevels.Add(priceLevel);
+                priceLevel -= priceStep;
+            }
+
+            //Вывод сетки на консоль
+
+            WriteLines();
+
+            Console.ReadKey();
+        }
+
+        static List<Decimal> priceLevels;
+
+        static void WriteLines()
+        {
+            Console.WriteLine("Количество уровней: " + priceLevels.Count.ToString());
+
+            Console.WriteLine("Уровни: ");
+            for (int i = 0; i < priceLevels.Count; i++)
+            {
+                Console.WriteLine(priceLevels[i]);
+            }
+        }
+    }
+}
+
