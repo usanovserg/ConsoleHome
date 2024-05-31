5a49dd8efca2a904dfc1e61f7fc7e1807d342dd3
bear.d3@outlook.com <bear.d3@outlook.com>
2024-05-31 10:04:03 +0300

    31.05.2024 Начало - Пустой проект 3

--- Program.cs
+++ Program.cs
@@ -6,38 +6,6 @@ namespace Levels
 {
     internal class Program
     {
-        /// <summary>
-        /// Метод 1. Генератор ценовых уровней
-        /// </summary>
-        /// <returns></returns>
-        static Decimal[] LevelsGen(Decimal[] levels, UInt32 numLevels, Decimal priceLow, Decimal stepLevels)
-        {
-            levels[0] = priceLow;
-
-            for (int i = 1; i < numLevels; i++)
-            {
-                levels[i] = levels[i - 1] + stepLevels;
-            }
-
-            return levels;
-        }
-
-        /// <summary>
-        /// Метод 2. Вывод ценовых уровней на консоль
-        /// </summary>
-        /// <param name="levels"></param>
-        /// <param name="numLevels"></param>
-        static void LevelsToConsole(Decimal[] levels, UInt32 numLevels)
-        {
-            Console.WriteLine("Ценовые уровни для робота:");
-
-            for (int i = 0; i < numLevels; i++)
-            {
-                Console.WriteLine(levels[i]);
-            }
-
-            Console.WriteLine();
-        }
 
         static void Main()
         {
