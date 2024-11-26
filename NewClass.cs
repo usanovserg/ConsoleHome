40b5ba516c9ab0b4f94fc8aa1962cce7c887200b
iapplegit <188614366+iapplegit@users.noreply.github.com>
2024-11-26 18:29:22 +0300

    Update NewClass.cs
    Update team number

--- NewClass.cs
+++ NewClass.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,7 @@ namespace ConsoleHome
     {
         public NewClass()
         {
-            Console.WriteLine("Команда разработчиков 2!");
+            Console.WriteLine("Команда разработчиков 3!");
         }
     }
 }
