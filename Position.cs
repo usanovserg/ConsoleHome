9ad2209f6caac80ba1604f2996d81ebb3b527018
nekhaev <ndn@mail.ru>
2024-12-03 02:04:28 +0300

    Добавил классы Trade и Positon и эмулятор добавления сделок в позицию по таймеру

---
+++ Position.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace ConsoleHome {
+    public class Position {
+        public string sec_code;
+        List<Trade> trades;
+        public decimal total_vol;
+        public decimal avg_price;
+        public decimal live_value;
+        public decimal fixed_value;
+        public decimal last_price;
+        public decimal closed_vol;
+        public Position(string sec_code) {
+            this.sec_code = sec_code;
+            total_vol = 0;
+            fixed_value = 0;
+            live_value = 0;
+            last_price = 0;
+            avg_price = 0;
+            trades = new List<Trade>();
+        }
+
+        public void OnTrade(Trade trade) {
+            if (trade == null) return;
+            trades.Add(trade);
+
+            decimal new_total_vol = total_vol + trade.vol;
+
+            // фиксируем часть позиции
+            if(Math.Sign(trade.vol) != Math.Sign(total_vol) && total_vol!=0) {
+                if(Math.Abs(total_vol) > Math.Abs(trade.vol)) {
+                    closed_vol += Math.Abs(trade.vol);
+                    fixed_value += trade.vol * (avg_price - trade.price);
+                }
+            } else {
+                if(new_total_vol != 0) {
+                    decimal total = total_vol * avg_price + trade.price * trade.vol;
+                    avg_price = total / new_total_vol;
+                } else {
+                    avg_price = 0;
+                }
+            }
+            total_vol = new_total_vol;
+        
+            last_price = trade.price;
+
+            live_value = (last_price-avg_price) * total_vol;
+        }
+        public override string ToString() {
+            return $"{sec_code} avg_price={avg_price:f2} vol={total_vol} last_price={last_price:f2} value={live_value+fixed_value:f2} live={live_value:f2} fixed={fixed_value:f2}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleHome;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+
+Random rnd = new Random();
+decimal currentPrice = 1000;
+Position p = new Position("ZZZ");
+void T_Elapsed(object? sender, ElapsedEventArgs e) {
+    Trade.Direction dir = (Trade.Direction)rnd.Next(1, 3);
+    currentPrice += rnd.Next(-20, 20);
+    decimal vol = rnd.Next(1, 10)*(dir==Trade.Direction.BUY?1:-1);
+    Trade t = new Trade(p.sec_code, currentPrice, vol, dir);
+    Console.WriteLine(t.ToString() );
+    p.OnTrade(t);
+    Console.WriteLine(p);
+}
+;
+
+Timer t = new Timer();
+t.Interval = 1000;
+t.AutoReset = true;
+t.Elapsed += T_Elapsed;
+
+t.Start();
+//Console.WriteLine(p);
+//p.OnTrade(new Trade(p.sec_code, 10, 5, Trade.Direction.BUY));
+//Console.WriteLine(p);
+//p.OnTrade(new Trade(p.sec_code, 11, -3, Trade.Direction.SELL));
+//Console.WriteLine(p);
+Console.ReadLine();
