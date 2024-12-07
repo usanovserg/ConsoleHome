d1fc9758873aa96b82fcdd96e51d6a56b65308c4
Pavel <Pavel@DESKTOP-457BB4C>
2024-12-07 14:52:04 +0500

    Urok_1_5

--- NewClass.cs
+++ NewClass.cs
@@ -4,13 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleHome
+namespace Urok_1_5
 {
-    public class NewClass
+    public class Trade
     {
-        public NewClass()
+        //============================================ Fields ==========================================
+
+        #region Fields
+
+        public decimal Price = 0;
+
+        public string SecCode = ""; // название инструмента
+
+        public string ClassCode = ""; // классификация биржы
+
+        public DateTime DateTime = DateTime.MinValue;
+
+        public string Portfolio = ""; // номер счета
+
+        #endregion
+
+        //=========================================== Properties ========================================
+
+        #region Properties
+        /// <summary>
+        /// Объем сделки
+        /// </summary>
+        public decimal Volume
         {
-            Console.WriteLine("Команда разработчиков 2!");
+            get
+            {
+                return _volume;
+            }
+            set
+            {
+                _volume = value;
+            }
         }
+        decimal _volume = 0;
+
+        #endregion
+
+        //============================================ Methods ==========================================
+
+        #region Methods
+
+
+
+        #endregion
+
+
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,114 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urok_1_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            levels = new List<decimal>();
+
+            WriteLine();
+
+            string str = ReadLine("Введите количество уровней: ");
+
+            contLevels = Convert.ToInt32(str);
+
+            str = ReadLine("Задайте верхнюю цену: ");
+
+            priceUp = decimal.Parse(str);
+
+            str = ReadLine("Введите шаг уровня: ");
+
+            StepLevel = decimal.Parse(str);
+
+            WriteLine();
+
+            Console.ReadLine();
+        }
+
+        //============================================ Fields ==========================================
+
+        #region Fields
+
+        static List<decimal> levels;
+        static decimal priceUp;
+        static int contLevels;
+
+        //==========================
+
+        static Trade trade = new Trade();
+
+
+        #endregion
+
+        //=========================================== Properties ========================================
+
+        #region Properties
+
+        static decimal StepLevel
+        {
+            get
+            {
+                return stepLevel;
+            }
+
+            set
+            {
+                if (value <= 100)
+                {
+                    stepLevel = value;
+
+                    decimal priceLevel = priceUp;
+
+                    for (int i = 0; i < contLevels; i++)
+                    {
+                        levels.Add(priceLevel);
+                        priceLevel -= stepLevel;
+                    }
+                }
+
+            }
+        }
+        static decimal stepLevel;
+
+        #endregion
+
+        //============================================ Methods ==========================================
+
+        #region Methods
+
+
+        static void WriteLine()
+        {
+            Console.WriteLine("Количество элементов в списке: " + levels.Count.ToString());
+
+            for (int i = 0; i < levels.Count; i++)
+            {
+                Console.WriteLine(levels[i]);
+            }
+        }
+
+        static string ReadLine(string message)
+        {
+            Console.WriteLine(message);
+
+            return Console.ReadLine();
+        }
+
+        static void Hhbjknk()
+        {
+            trade.Price = 2225;
+
+            trade.Volume = 232325656;
+
+        }
+
+        #endregion
+    }
+}
