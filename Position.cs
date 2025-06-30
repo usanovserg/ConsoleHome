a57e7057d20bbab114be0219108027b7640c1a72
DESKTOP-5E7409A\Alex <docent748@gmail.com>
2025-06-30 20:07:06 +0300

    New branch

---
+++ Position.cs
@@ -0,0 +1,48 @@
+﻿using System.Timers;
+using System.Threading;
+using System.Threading.Tasks;
+using System;
+using Timer = System.Timers.Timer;
+
+namespace ConsoleHome
+{
+    public class Position
+    {
+        public Position()
+        {
+            Timer timer = new Timer();
+
+            timer.Interval = 5000;
+
+            timer.Elapsed += Timer_Elapsed;
+
+            timer.Start();
+        }
+
+        Random random = new Random();
+
+        private void Timer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            Trade trade = new Trade();
+
+            int num = random.Next(-10, 10);
+
+            if (num > 0)
+            {
+                // Сделка в лонг
+            }
+            else if (num < 0)
+            {
+                // Сделка в шорт
+            }
+
+            trade.Volume = Math.Abs(num);  
+
+            trade.Price = random.Next(70000,80000);
+
+            string str = "Volume = " + trade.Volume. ToString() + " / Price = " + trade.Price.ToString();
+
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ Trade.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHome
+{
+    public class Trade
+    {
+        //======================================================= Fiels ============================================================== 
+        #region Fields
+
+        /// <summary>
+        /// Цена инструмента
+        /// </summary>
+        public decimal Price = 0;
+
+        public string SecCode = "";
+
+        public string ClassCode = "";
+
+        public DateTime DateTime = DateTime.MinValue;
+
+        public string Portfolio = "";
+
+        #endregion
+
+        //======================================================= Properties =========================================================
+        #region Properties
+
+        /// <summary>
+        /// Объем сделки
+        /// </summary>
+        public decimal Volume
+        {
+            get
+            {
+                return _volume;
+            }
+
+            set
+            {
+                _volume = value;
+            }
+        }
+        decimal _volume = 0;
+
+        #endregion
+    }
+}
