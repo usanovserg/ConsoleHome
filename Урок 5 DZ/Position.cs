30f4def0a20a6493acfa842e8e58541885f4b838
Dmitriy Ivanov <divsar@yandex.ru>
2025-07-09 17:13:23 +0400

    Домашнее задание 1_5

---
+++ Урок 5 DZ/Position.cs
@@ -0,0 +1,54 @@
+﻿using MyConsole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using static MyConsole.Trade;
+
+namespace MyConsole
+{
+    public class Position
+    {
+        public Position()
+        {
+            Timer timer = new Timer();
+
+            timer.Interval = 3000;
+
+            timer.Elapsed += NewTrade;
+
+            timer.Start();
+        }
+
+        Random random = new Random();
+
+        private void NewTrade(object sender, ElapsedEventArgs e)
+        {
+            Trade trade = new Trade();
+
+            int num = random.Next(-10, 10);
+            
+            if (num > 0)
+            {
+                trade.typeOrder = TypeOrder.Long;
+
+            }
+            else if (num < 0)
+            {
+                trade.typeOrder = TypeOrder.Short;
+            }
+
+            trade.Volume = Math.Abs(num);
+
+            trade.Price = random.Next(70000, 80000);
+
+            string str = $"Volume = {trade.Volume} / Price = {trade.Price} / OrderType = {trade.typeOrder}";
+
+            Console.WriteLine(str);
+        }
+
+
+    }
+}
---
+++ Урок 5 DZ/Trade.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyConsole;
+
+
+namespace MyConsole
+{
+
+    public class Trade
+    {
+        #region Fields
+
+        /// <summary>
+        /// Цена инструмента
+        /// </summary>
+        public decimal Price = 0;
+        /// <summary>
+        /// Название инструмента
+        /// </summary>
+        public string Seccode = "";
+        /// <summary>
+        /// код инструмента
+        /// </summary>
+        public string ClassCode = "";
+        /// <summary>
+        /// время сделки
+        /// </summary>
+        public DateTime DateTime = DateTime.MinValue;
+        /// <summary>
+        /// номер счета
+        /// </summary>
+        public string Portfolio = "";
+        /// <summary>
+        /// Код клиента
+        /// </summary>
+        public int ClientsCode = 0;
+        /// <summary>
+        /// Тип ордера
+        /// </summary>
+        public TypeOrder typeOrder;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Объем сделки
+        /// </summary>
+        public decimal Volume
+        {
+            get
+            {
+                return _volume;
+            }
+
+            set
+            {
+                _volume = value;
+            }
+        }
+        
+        private decimal _volume = 0;
+
+        #endregion
+
+        
+    }
+}
