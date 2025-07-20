9af8746ecfd1642bde719176c6a111adbb7dcb28
user <anton.borisov.m@gmail.ru>
2025-07-20 08:17:21 +0300

    Регионы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,114 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            levels = new List<decimal>();
+
+            WriteLine();
+
+            string str = ReadLine("Введите количество уровней: ");
+
+            countLevels = Convert.ToInt32(str);
+
+            str = ReadLine("Задайте верхнюю цену: ");
+
+            priceUp = decimal.Parse(str);
+
+            str = ReadLine("Введите шаг уровня: ");
+
+            StepLevel = decimal.Parse(str);
+
+            str = Console.ReadLine();
+
+            WriteLine();
+
+            Console.ReadLine();
+
+        }
+
+        //----------------------------------------------- Fields ---------------------------------------------------- 
+        #region Filds
+
+        static int countLevels;
+
+        static decimal priceUp;
+
+        static decimal priceLevel = priceUp;
+
+        static decimal stepLevel;
+
+        #endregion
+        //----------------------------------------------- Fields ----------------------------------------------------
+
+        //----------------------------------------------- Properties ------------------------------------------------
+        #region Properties
+
+        public static decimal StepLevel
+        {
+            get
+            {
+                return StepLevel;
+            }
+
+            set
+            {
+                if (value <= 100)
+                {
+                    stepLevel = value;
+
+                    decimal priceLevel = priceUp;
+
+                    for (int i = 0; i < countLevels; i++)
+                    {
+                        levels.Add(priceLevel);
+
+                        priceLevel -= stepLevel;
+                    }
+                }
+
+            }
+        }
+
+        #endregion
+        //----------------------------------------------- Properties ------------------------------------------------
+
+        static List<decimal> levels;
+
+        //----------------------------------------------- Methods ---------------------------------------------------
+        #region Methods
+
+        static void WriteLine()
+        {
+            Console.WriteLine("Кол-во элементов в списке: " + levels.Count.ToString());
+            for (int i = 0; i < levels.Count; i++)
+            {
+                Console.WriteLine(levels[i]);
+            }
+            Console.ReadLine();
+            //1            
+            //2
+            //3
+
+        }
+
+        static string ReadLine(string message)
+        {
+            Console.WriteLine(message);
+
+            return Console.ReadLine();
+        }
+
+
+        #endregion
+        //----------------------------------------------- Methods ---------------------------------------------------
+    }
+}
