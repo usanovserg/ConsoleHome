412ce7bffcbcd4213d2b650ae3470d7ac33e5f4f
iscander <mitrichdo@gmail.com>
2025-08-05 19:21:55 +0300

    Добавлен код

--- Program.cs
+++ Program.cs
@@ -1,37 +1,94 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
 
-
-
-using System;
-
-public class Program
+namespace MyConsole
 {
-    static void Main()
+    public class Program
     {
-        Console.WriteLine("Hello, World!");
-
-        while (true)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Введите число: ");
-            string? str = Console.ReadLine();
-
-
-            if (int.TryParse(str, out int num) == true)
-            {
-                if (num != 0)
-                {
-                    decimal count = 9 / num;
-
-                    Console.WriteLine("result = " + count);
-                }
-                else Console.WriteLine("На ноль делить нельзя!");
-            }
-            else  Console.WriteLine("введите корректное значение");
+            levels = new List<decimal>();
+            WriteLine();
+            string str = ReadLine("Введите количество уровней: ");
+            countLevels = Convert.ToInt32(str);
+            str = ReadLine("Задайте верхнюю цену: ");
+            priceUp = decimal.Parse(str);
+            str = ReadLine("Введите шаг уровня: ");
+            StepLevel = decimal.Parse(str);
+            WriteLine();
+            Console.ReadLine();
         }
 
+        
+        #region Fields
+        static List<decimal> levels;
+
+        static decimal priceUp;
+
+        static internal int countLevels;
+
+        Trade trade = new Trade();
+
+
+        #endregion
+
+        #region Properties
+    public static decimal StepLevel
+        {
+            get
+            {
+                return stepLevel;
+            }
+            set
+            {
+                if (value <= 100)
+                {
+                    stepLevel = value;
+                    decimal priceLevel = priceUp;
+
+                    for (int i = 0; i < countLevels; i++)
+                    {
+                        levels.Add(priceLevel);
+                        priceLevel -= stepLevel;
+                    }
+                }
+            }
+        }
+        static decimal stepLevel;
+        #endregion
+
+        #region Methods
+
+        static void WriteLine()
+        {
+            Console.WriteLine("Количество элементов в списке: " + levels.Count.ToString());
+
+            for (int i = 0; i < levels.Count; i++)
+            {
+                Console.WriteLine(levels[i]);
+            }
+        }
+        static string ReadLine(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+        #endregion
+    }
+
+    class Trade
+    {
+        #region Fields
+        decimal Price = 0;
+        decimal Volume = 0;
+        string SecCode = "";
+        string ClassCode = "";
+        DateTime DateTime = DateTime.MinValue;
+        string Portfolio = "";
+        #endregion
     }
 }
-
-
-
-
