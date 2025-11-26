7ce8aeb4534b7d6a4405839f3d0eed9ac015e57d
Андрей Заведеев <andrew_zavedeev@mail.ru>
2025-11-26 18:36:15 +0300

    Добавил свой комментарий Andrew и внес изменения в NewClass

--- Program.cs
+++ Program.cs
@@ -35,6 +35,8 @@ namespace MyConsole
 
         } // iofgijfpogj
 
+        // comment Andrew Zavedeev
+
         //----------------------------------------------- Fields ---------------------------------------------------- 
         #region Filds
 
