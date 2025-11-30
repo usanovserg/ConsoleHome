1afba2871ffeacff894e4f18b36df9775999a3ee
Андрей Заведеев <andrew_zavedeev@mail.ru>
2025-11-30 17:26:22 +0300

    Измена

---
+++ Level.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHome
+{
+    public class Level
+    {
+
+        //public Level(decimal price) { PriceLevel = price; }
+
+        public Level() { }
+        
+        //----------------------------------------------- Fields ---------------------------------------------------- 
+
+        #region Fields
+
+        public decimal PriceLevel = 0;
+
+        public decimal LotLevel = 0;
+        
+        public decimal Volume = 0;
+
+        #endregion
+
+
+    }
+}
