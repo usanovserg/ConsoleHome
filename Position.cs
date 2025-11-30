738963dbf0e238be234a76a3980ccb1460c71bc3
L R <winapps81@hotmail.com>
2025-11-30 23:17:22 +0300

    + class Position. Ошибок нет, но на консоль не выводит. Проблема с таймером уже при создании. При отладке не заходит в автосозданный timer.Elapsed()

---
+++ Position.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace ConsoleHome
+{
+    public class Position
+    {
+        public Position()
+        {
+            Timer timer = new Timer();      // создаем объект таймер
+
+            timer.Interval = 5000;          // задаём свойство: интервал 1000 мсек = 1 сек
+
+            timer.Elapsed += NewTrade;      // наш таймер раз в 1 сек будет вызывать метод Timer_Elapsed
+
+            timer.Start();                  // запускаем таймер. Он в параллельном потоке: private void Timer_Elapsed(* * *) начинает раз в секунду вызывать Timer_Elapsed
+
+        }
+
+        Random random = new Random();
+
+        private void NewTrade(object? sender, ElapsedEventArgs e)      // это само создалось при заполнении timer.Elapsed += Timer_Elapsed;
+        {
+            Trade trade = new Trade();
+
+            int num = random.Next(-10, 10);
+
+            if (num > 0)
+            {
+                // сделка в лонг
+            }
+            else if (num < 0) 
+            {
+                // сделка в шорт
+            }
+
+            trade.Volume = Math.Abs(num);
+
+            trade.Price = random.Next(70000, 80000);
+
+            string str = "Volume = " + trade.Volume.ToString() + "\tPrice = " + trade.Price.ToString();
+            Console.WriteLine(str);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,19 @@
-﻿using ConsoleHome;  // подключен класс
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using ConsoleHome;  // подключен namespace ConsoleHome
 
-namespace MyConsole
+namespace ConsoleHome
 {
     internal class Program
     {
         static void Main(string[] args)
-        {            
+        {
+            Position position = new Position();
+
+            /*
             levels = new List<Level>();
 
             WriteLine();
@@ -33,7 +36,7 @@ namespace MyConsole
                         
             WriteLine();
 
-            //Console.ReadLine(); 
+            */
 
         }
 
