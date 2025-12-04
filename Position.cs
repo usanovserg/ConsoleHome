129225f55b5a90a4ba25efc6d3e8fc7c3e6e04d7
Dmitry Boev <dmitry.boev@hotmail.com>
2025-12-04 13:21:46 +0300

    Создание нового класса

---
+++ Position.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;    
+
+namespace ConsoleHome
+{
+    public class Position
+    {
+        public Position()
+        {
+            Timer timer = new Timer();
+
+            timer.Interval = 1000; // Интервал в миллисекундах (1 секунда)
+
+            timer.Elapsed += Timer_Elapsed; // Подписка на событие Elapsed
+
+            timer.Start(); // Запуск таймера
+
+        }
+        Random random = new Random();
+        
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Trade trade = new Trade
+            {
+                int num = random.Next(-10, 10);
+
+                if (num > 0)
+            {
+                // Сделка Long
+
+            }
+
+
+                else if (num < 0)
+            {
+                // Сделка Short
+
+            }
+                trade.Volume = Math.Abs(num);
+
+                trade.Price = Random.Next(70000, 80000);
+
+            string str = "Volume = " + trade.Volume.ToString()
+
+
+            Console.WriteLine(str);
+
+        }
+    }
+}
