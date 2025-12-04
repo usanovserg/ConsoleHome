07371b0de869d518aae0fd1c0e565f4b488420d1
SergKalga <sakalgin@gmail.com>
2025-12-04 17:03:24 +0500

    Добавил в новую позицию вычисление средней цены позиции (AveregePositionPrice), кол-во лотов в текущей позиции (с учетом переворотов из Лонга в Шорт и обратно), с выводом в консоль.

--- Position.cs
+++ Position.cs
@@ -16,7 +16,7 @@ namespace ConsoleHome
         {
             System.Timers.Timer timer = new System.Timers.Timer();
 
-            timer.Interval = 1000;
+            timer.Interval = 5000;
 
             timer.Elapsed += NewTrade;
 
@@ -47,34 +47,66 @@ namespace ConsoleHome
         {
             Trade trade = new Trade();
 
+            decimal longAndShort;
+
             string tapeDeal = "";
-
-            int num = random.Next(-10, 10);
-
-            if (num > 0)
-            {
-                // Сделка в лонг;
-                MyTransaction _deal = MyTransaction.Bay;
-                tapeDeal = "Deal Bay >>> ";
-
-            }
-            else if (num < 0)
-            {
-                // Сделка в шорт;
-                MyTransaction _deal = MyTransaction.Sell;
-                tapeDeal = "Deal Sell <<< ";
-            }
-            else { tapeDeal = "Deal Not ------ "; }
-
-            trade.Volume = Math.Abs(num);
 
             trade.Price = random.Next(70000, 80000);
 
+            int num = random.Next(-10, 10);
 
-            string str = tapeDeal + "Volume = " + trade.Volume.ToString() + " / Price = " + trade.Price.ToString();
 
 
-            Console.WriteLine(str);
+            if ((VolumeLots >= 0 && num > 0) || (VolumeLots <= 0 && num < 0))
+            {
+                // Вычисление средней цены для ранее открытой позиции с увеличением лотов в такой позиции (без изменения направления позиции);
+                AveregePositionPrice = (Math.Abs(VolumeLots) * Math.Abs(AveregePositionPrice) + Math.Abs(num) * trade.Price) / (Math.Abs(VolumeLots) + Math.Abs(num));
+            }
+            // Объем текущей позиции (VolumeLots) меньше нового объема сделки (num), что позицию "переворачивает" (из Лонга в Шорт или наоборот)  - брать цену инструмента (последней сделки);
+            else if (num != 0 && Math.Abs(VolumeLots) < Math.Abs(num))
+            { AveregePositionPrice = trade.Price; }
+
+            // После добавления к объему текущей позиции (VolumeLots) нового объема сделки (num), позиция закрыта "в ноль" (нет средней цены позиции);
+            else if ( (VolumeLots + num) == 0 ) { AveregePositionPrice = 0; }
+            // Для иных случаев средняя цена не меняется;
+            
+
+            if (num > 0)
+            {                
+                // Сделка в лонг;
+                tapeDeal = "Deal Bay >>> ";
+
+                longAndShort = VolumeLots + num;
+            }
+            else if (num < 0)
+            {
+                // Сделка в шорт;                
+                tapeDeal = "Deal Sell <<< ";
+
+                longAndShort = VolumeLots + num;
+            }
+            else
+            {
+                tapeDeal = "Deal Not ------ ";
+                longAndShort = VolumeLots;
+            }
+
+            trade.Volume = Math.Abs(num);
+
+            VolumeLots = longAndShort;
+            
+
+            string str1 = tapeDeal + "Volume = " + trade.Volume.ToString() + " / Price = " + trade.Price.ToString();
+
+            string str2 = "Current position (lot) = " + VolumeLots;
+
+            string str3 = "Averege price position (all lots) " + AveregePositionPrice;
+
+
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+            Console.WriteLine(str3);
+            Console.WriteLine();
         }
     }
 }
