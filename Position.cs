3bbb99968f09dc8af9a1a8fd0951df24c204da52
Dmitry Boev <dmitry.boev@hotmail.com>
2025-12-09 20:00:45 +0300

    Новый класс Trade

--- Position.cs
+++ Position.cs
@@ -6,7 +6,8 @@ using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-using System.Timers;    
+using System.Timers;
+using Timer = System.Timers.Timer;
 
 namespace ConsoleHome
 {
